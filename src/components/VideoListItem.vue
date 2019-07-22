eb8a90e8aa54f0d24ea9917cab02f219ec233d98
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2019-07-22 13:43:12 +0200

    Add thumbnail and styiling VideoListItem

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,5 +1,8 @@
 <template>
-	<li class="list-group-item">{{ video.snippet.title }}</li>
+	<li class="list-group-item media">
+		<img class="mr-3" :src="thumbnailUrl" :alt="videoTitle" />
+		<div class="media-body">{{ videoTitle }}</div>
+	</li>
 </template>
 
 <script>
@@ -7,6 +10,25 @@ export default {
 	name: "VideoListItem",
 	props: {
 		video: Object
+	},
+	computed: {
+		videoTitle() {
+			return this.video.snippet.title;
+		},
+		thumbnailUrl() {
+			return this.video.snippet.thumbnails.default.url;
+		}
 	}
 };
 </script>
+
+<style scoped>
+li {
+	display: flex;
+	cursor: pointer;
+}
+
+li:hover {
+	background-color: lightgray;
+}
+</style>
